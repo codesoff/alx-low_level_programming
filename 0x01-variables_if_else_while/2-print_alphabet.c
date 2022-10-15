effaff86e1bf894f735a53fadd72ec00800c1d05
ZoLa <zolaabe506@gmail.com>
2022-10-15 12:05:41 -0700

    alphabet

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,16 +2,15 @@
 #include <stdlib.h>
 #include <time.h>
 /**
-* main - Entry point
-* 
+* main - Entry point 
 * print lowercase alphabet
 * Return: 0
 */
 int main(void)
 {
-	char ch='a';
-	for(ch = 'a';ch <= 'z';ch++)
-		putchar(ch);
-	putchar('\n');
-	return(0);
+	char ch = 'a';
+	for (ch = 'a';ch <= 'z'; ch++)
+		putchar(ch); 
+	putchar('\n'); 
+	return (0);
 }
