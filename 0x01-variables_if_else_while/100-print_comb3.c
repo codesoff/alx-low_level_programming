f3ab74bd88c67f42b9c6aa4758ce308bd3cb4803
ZoLa <zolaabe506@gmail.com>
2022-10-16 07:17:38 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print all possible different combinations of two digits
+ * Return: 0
+ */
+int main(void)
+{
+int i, a, b;
+i = 0;
+while (i < 90)
+{
+a = i % 10;
+b = i / 10;
+if (b < a) {
+putchar(b + '0');
+putchar(a + '0');
+if (i < 89) {
+putchar(',');
+putchar(' ');
+}
+i++;
+}
+}
+putchar('\n');
+return (0);
+}
