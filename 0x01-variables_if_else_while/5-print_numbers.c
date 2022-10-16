7d400d6a36c6c9fd64a20499fa4eeccfe6456c9a
ZoLa <zolaabe506@gmail.com>
2022-10-16 01:27:32 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * print all single digit numbers of base 10
+ * Return: 0
+ */
+int main(void)
+{
+int i;
+for (i = 0; i <= 9; i++)
+	printf("%d", i);
+putchar('\n')
+return (0);
+}
