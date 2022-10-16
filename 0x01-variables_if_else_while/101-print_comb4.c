cf18013a10d36d34f2e329ecedaa03d9baf059b7
ZoLa <zolaabe506@gmail.com>
2022-10-16 09:03:35 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * prints all possible different combinations of three digits
+ * Return: 0
+ */
+int main(void)
+{
+int i, a, b, c;
+for (i = 0; i <= 800; i++)
+{
+a = i/100;
+b=(i/10)%10;
+c=i%10;
+if (a<b && b<c)
+{
+putchar(a + '0');
+putchar(b + '0');
+putchar(c + '0');
+if (i < 789)
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
