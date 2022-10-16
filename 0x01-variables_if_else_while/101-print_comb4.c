2d65ae21851333fa598549dad7334acc53ebac64
ZoLa <zolaabe506@gmail.com>
2022-10-16 09:05:40 -0700

    alphabet

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,10 +9,10 @@ int main(void)
 int i, a, b, c;
 for (i = 0; i <= 800; i++)
 {
-a = i/100;
-b=(i/10)%10;
-c=i%10;
-if (a<b && b<c)
+a = i / 100;
+b= (i / 10) % 10;
+c= i % 10;
+if (a < b && b < c)
 {
 putchar(a + '0');
 putchar(b + '0');
