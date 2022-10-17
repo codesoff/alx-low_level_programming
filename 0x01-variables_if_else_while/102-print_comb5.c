970f55cfc9b56f74f6454f3ddcd16709e7a3447b
ZoLa <zolaabe506@gmail.com>
2022-10-17 00:35:01 -0700

    alphabet

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,10 +11,10 @@ for (i = 0; i <= 99; i++)
 {
 	a = i / 10;
 	b = i % 10;
-	for (j = 10; j <= 99; i++) {
+	for (j = 0; j <= 99; i++) {
 	c = j / 10;
 	d = j % 10;
-	if (a < c || (a == c && b <d)) {
+	if (a < c || (a == c && b < d)) {
 		putchar(a + '0');
 	putchar(b + '0');
 	putchar(' ');
