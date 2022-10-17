484c4727748f6c715cc60dac101bb2e9a09e6233
ZoLa <zolaabe506@gmail.com>
2022-10-17 00:48:18 -0700

    alphabet

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Write a program that prints all possible combinations of two two-digit numbers.
+ * Write a program that prints all possible combinations
+ * of two two-digit numbers.
  * Return: 0
  */
 int main(void)
@@ -11,18 +12,19 @@ for (i = 0; i <= 99; i++)
 {
 	a = i / 10;
 	b = i % 10;
-	for (j = 0; j <= 99; j++) {
+	for (j = 0; j <= 99; j++)
+	{
 	c = j / 10;
 	d = j % 10;
-	if (a < c || (a == c && b < d)) {
+	if (a < c || (a == c && b < d))
+	{
 		putchar(a + '0');
 	putchar(b + '0');
 	putchar(' ');
 	putchar(c + '0');
 	putchar(d + '0');
-	if (a == 9 && b ==8) {
+	if (a == 9 && b == 8)
 	break;
-	}
 	putchar(',');
 	putchar(' ');
 	} } 
