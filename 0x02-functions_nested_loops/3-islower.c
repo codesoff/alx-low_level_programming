2609b93735504f9b49eca00cd19d69516dd2ea2f
ZoLa <zolaabe506@gmail.com>
2022-10-19 21:31:18 -0700

    ptchar

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _islower - checks lowercase
+ * lowercase character
+ * Return: 0
+ */
+int _islower(int c)
+{
+char c;
+if (isupper(c))
+	_putchar('1');
+else 
+	_putchar('0');
+}
