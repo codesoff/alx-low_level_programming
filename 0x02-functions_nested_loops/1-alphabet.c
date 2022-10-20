78901e5919bc342d5bf0f2bc97385d647d6674e8
ZoLa <zolaabe506@gmail.com>
2022-10-19 20:00:35 -0700

    ptchar

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
- * print_alphabet
+ * print_alphabet 
  * prints alphabet,in lowercase
  * Return: no return
  */
-void print_alphabet(void);
+int main(void);
 {
 int ch;
 for (ch = 'a'; ch <= 'z'; ch++)
 	_putchar(ch);
 _putchar('\n');
+return (0);
 }
