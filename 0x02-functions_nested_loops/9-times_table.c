f6de034a05bf3720427936ffb82e2ae0f9fb3399
ZoLa <zolaabe506@gmail.com>
2022-10-22 01:22:42 -0700

    ptchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,27 +1,33 @@
 #include "main.h"
-// Online C compiler to run C program online
-#include <stdio.h>
-
-int main() {
-    int a, b, c;
-    for (a = 0; a <= 9; a++)
-    {
-        for (b = 0; b <= 9; b++)
-        {
-        c = a * b;
-         putchar(',');
-        putchar(' ');
-         putchar(c + '0');
-        if ( c >+ 10)
-        {
-            putchar((c / 10) + '0');
-            putchar((c % 10) + '0');
-        }
-      // putchar(' ');
-      if(a <= 9 && b <= 9)
-
-    }
-    putchar('\n');
-    }
-    return 0;
+/**
+ * times_table - prints 9 times
+ * prints 9 times table
+ * Return: 0
+ */
+void times_table(void)
+{
+int a, b, c;
+for (a = 0; a <= 9; a++)
+{
+for (b = 0; b <= 9; b++)
+{
+c = a * b;
+if (c <= 9)
+{
+_putchar(' ');
+_putchar(c + '0');
+}
+else if (c >= 9)
+{
+_putchar((c / 10) + '0');
+_putchar((c % 10) + '0');
+}
+if (b <= 8)
+{
+_putchar(',');
+}
+_putchar(' ');
+}
+_putchar('\n');
+}
 }
