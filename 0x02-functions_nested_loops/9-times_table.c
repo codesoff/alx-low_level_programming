ad2663dd70396575667434b8099270389091602e
ZoLa <zolaabe506@gmail.com>
2022-10-22 01:42:25 -0700

    ptchar

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -9,9 +9,10 @@ void times_table(void)
 int a, b, c;
 for (a = 0; a <= 9; a++)
 {
-for (b = 0; b <= 9; b++)
+	_putchar('0');
+	for (b=1; b <= 9; b++)
 {
-c = a * b;
+ic = a * b;
 if (c <= 9)
 {
 _putchar(' ');
