b6e57bce1cb03a9332c6dfc49277d537ca3ba227
ZoLa <zolaabe506@gmail.com>
2022-10-22 03:25:47 -0700

    ptchar

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * print_to_98 - prints all natural numbers from n to 98
+ * @n: natural numbers
+ * Return: 0
+ */
+void print_to_98(int n)
+{
+while (n > 98)
+{
+printf("%d", n--);
+if (n > 97)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+while (n < 99)
+{
+printf("%d", n++);
+if (n < 99)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
+}
