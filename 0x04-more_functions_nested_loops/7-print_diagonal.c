bd26df2cf8d83302098c207fc8a615162709538c
ZoLa <zolaabe506@gmail.com>
2022-10-23 13:18:15 -0700

    0x03-debugging

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line 
+ * @n: integer number
+ * Return: 0
+ */
+void print_diagonal(int n)
+{
+	int i;
+	if (n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			_putchar(\);
+					_putchar('\n');
+					_putchar(' ');
+					}
+					}
+					}
