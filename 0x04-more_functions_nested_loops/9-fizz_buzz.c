489588103d05df41fe3042513d11c1aa0904ea1e
ZoLa <zolaabe506@gmail.com>
2022-10-23 14:48:42 -0700

    0x03-debugging

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - prints numbers frfom 1 - 100
+ * multiple three print Fizz, five Buzz, both FizzBuzz
+ * Return: 0
+ */
+int main(void)
+{
+	int Fizz, Buzz, i;
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0)
+			printf("Fizz");
+		else if (i % 5 == 0)
+			printf("Buzz");
+		else if (i % 15 = 0)
+			printf("FizzBuzz");
+		else
+			printf("%i", i);
+		printf(" ");
+	}
+	printf("\n");
+}
