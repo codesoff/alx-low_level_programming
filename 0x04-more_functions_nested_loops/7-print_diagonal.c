61bf871061afb3e7fa6018bea6cf1736b4209d0b
ZoLa <zolaabe506@gmail.com>
2022-10-23 13:42:43 -0700

    0x03-debugging

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -6,13 +6,16 @@
  */
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 	if (n <= 0)
 		_putchar('\n');
 	for (i = 0; i < n; i++)
 		{
-			_putchar(' ');
-					_putchar('\\');
+			for(j = 0; j < i; j++)
+			{
+				_putchar(' ');
+			}
+			_putchar('\\');
 					_putchar('\n');
 					}
 }
