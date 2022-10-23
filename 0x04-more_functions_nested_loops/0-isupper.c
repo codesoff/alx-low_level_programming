2260777be7d700d2e6447855c273931c2cb29ad6
ZoLa <zolaabe506@gmail.com>
2022-10-23 10:55:25 -0700

    0x04-more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    char c;
+
+    c = 'A';
+    printf("%c: %d\n", c, _isupper(c));
+    return (1);
+    c = 'a';
+    printf("%c: %d\n", c, _islower(c));
+    return (0);
+}
