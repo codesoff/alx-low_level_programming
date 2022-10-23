1f814e2890df7b770d6db279b87cf864e4209d7a
ZoLa <zolaabe506@gmail.com>
2022-10-23 12:27:04 -0700

    0x03-debugging

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,9 @@
 void more_numbers(void)
 {
 	char i, j;
-	for (j = 0; j <= 10; j++)
+	for (j = '0'; j <= '10'; j++)
 	{
-		for (i = 0; i <= 14; i++)
+		for (i = '0'; i <= '14'; i++)
 		{
 			_putchar(i);
 			_putchar('\n');
