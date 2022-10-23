c2966d23dba8f193ce99f2951c1160ad8c5f0c1e
ZoLa <zolaabe506@gmail.com>
2022-10-23 12:21:52 -0700

    0x03-debugging

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * more_numbers - that prints 10 times
+ * Return: 0
+ */
+void more_numbers(void)
+{
+	char i, j;
+	for (j = 0; j <= 10; j++)
+	{
+		for (i = 0; i <= 14; i++)
+		{
+			_putchar(i);
+			_putchar('\n');
+		}
+		_putchar('\n');
+	}
