c69cdad2969c6469dd5c2fa20bdc562e86cebc71
ZoLa <zolaabe506@gmail.com>
2022-10-24 08:22:48 -0700

    0x05-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,14 @@
 #include "main.h"
 /**
  * function that prints a string
+ * Description: prints a string
  * @str: pointer
  * Return: 0
  */
 void _puts(char *str)
 {
 	int i;
-	for (i = 0; str[i] != '\0'; i++)
+	for(i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
 	}
