00fcc1e633418f1cdd3cb6b596466f6a95252e07
ZoLa <zolaabe506@gmail.com>
2022-10-24 01:16:17 -0700

    0x03-debugging

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -2,7 +2,6 @@
 #include "main.h"
 /**
  * main - Entry point
- * Description: prints 1 - 100
  * multiple three print Fizz, five Buzz, both FizzBuzz
  * Return: 0
  */
@@ -11,12 +10,12 @@ int main(void)
 	int i;
 	for (i = 1; i <= 100; i++)
 	{
-		if (i % 3 == 0)
-			printf("Fizz");
+		if (i % 15 == 0)
+			printf("FizzBuzz");
 		else if (i % 5 == 0)
 			printf("Buzz");
-		else if (i % 15 == 0)
-			printf("FizzBuzz");
+		else if (i % 3 == 0)
+			printf("Fizz");
 		else
 			printf("%i", i);
 		if (i < 100)
