12af8abd48cbc6e6171b39a58cdee5cdb0712240
ZoLa <zolaabe506@gmail.com>
2022-10-24 07:18:07 -0700

    0x05-pointers_arrays_strings

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * that takes a pointer to an int as parameter
+ * and updates the value
+ * @n: pointer
+ * Return: 0
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef _MATH_H_
+#define _MAIN_H_
+
+void reset_to_98(int *n);
+
+#endif
