fb740694dc110bbb95623841bcabb57b17058636
ZoLa <zolaabe506@gmail.com>
2022-10-28 05:45:06 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "stdio.h"
+/**
+ * *_strcat - function that concatenates two strings
+ * @dest: string
+ * @src: string
+ * Return: 0
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a, b;
+	for (a = 0; dest[a] != '\0'; a++)
+
+		for (b = 0; src[b] != '\0'; c++)
+		{
+			dest[a] = src[b];
+			a++;
+		}
+	dest[a] = '\0';
+	return (dest);
+}
