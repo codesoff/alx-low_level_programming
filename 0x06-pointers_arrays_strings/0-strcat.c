b85175cca875ad5c4b22c1072a89fdbd6d78a3ff
ZoLa <zolaabe506@gmail.com>
2022-10-28 05:53:08 -0700

    0x06-pointers_arrays_strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -10,7 +10,7 @@ char *_strcat(char *dest, char *src)
 	int a, b;
 	for (a = 0; dest[a] != '\0'; a++)
 
-		for (b = 0; src[b] != '\0'; c++)
+		for (b = 0; src[b] != '\0'; b++)
 		{
 			dest[a] = src[b];
 			a++;
