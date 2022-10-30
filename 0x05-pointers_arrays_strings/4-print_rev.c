9c1121e25e45c708dc81027dbb7a769da74a677a
ZoLa <zolaabe506@gmail.com>
2022-10-30 05:12:50 -0700

    0x06-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -6,9 +6,9 @@
  */
 void print_rev(char *s)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (i >= 0; s[i] != '\0'; i++)
 		for (i--; i >= 0; i--)
 			_putchar(s[i]);
 	_putchar('\n');
