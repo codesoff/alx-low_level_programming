3a2353987f369318bca59ea61c1c34b09c9226d6
ZoLa <zolaabe506@gmail.com>
2022-10-31 00:16:37 -0700

    0x06-pointers_arrays_strings

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,10 +14,7 @@ void puts_half(char *str)
 		if (str[count] == '\0')
 			break;
 	}
-	if (count % 2 == 1)
-		i = count / 2;
-	else
-		i = (count - 1) / 2;
+	i = (count - 1) / 2;
 	for (i++; i < count; i++)
 		_putchar(str[i]);
 	_putchar('\n');
