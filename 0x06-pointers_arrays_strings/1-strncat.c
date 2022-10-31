5dfbce82ea57db12870b326f9aeccf1df6411c5c
ZoLa <zolaabe506@gmail.com>
2022-10-31 05:37:47 -0700

    0x06-pointers_arrays_strings

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * *_strncat - function that concatenates two strings
+ * @dest: string
+ * @src: string
+ * @n: length of byte
+ * Return: dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int 
