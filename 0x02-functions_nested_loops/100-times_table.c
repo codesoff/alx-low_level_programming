756acdd7ef5a4c6bd11b967fcadcb7b5c8d051f2
ZoLa <zolaabe506@gmail.com>
2022-10-31 02:44:54 -0700

    0x02-functions_nested_loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_times_table - a function
+ * that prints the n times table
+ * @n: input number
+ * Return: 0
+ */
+void print_times_table(int n)
+{
+int a, b;
+if (n < 15 && n > 0)
+
+}
