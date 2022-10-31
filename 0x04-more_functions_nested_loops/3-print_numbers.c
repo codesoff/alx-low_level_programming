4772b6e00cb0ea3101d9f343748338a30d533576
ZoLa <zolaabe506@gmail.com>
2022-10-31 03:52:01 -0700

    0x04-more_functions_nested_loops

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,18 +1,19 @@
 #include "main.h"
 
 /**
- * print_numbers - prints numbers
- * 
+ * print_numbers - prints 0 - 9
+ *
  * Return: void
  */
 
+
 void print_numbers(void)
 {
-	char i;
-	
-	for (i = '0'; i <= '9'; i++)
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(i);
+		_putchar(c);
 	}
 	_putchar('\n');
 }
