bc6eba52e8cff3e4d630587f1482db47116def73
ZoLa <zolaabe506@gmail.com>
2022-11-03 08:09:22 -0700

    0x08-recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -11,6 +11,9 @@ int _pow_recursion(int x, int y)
 {
 	if (y < 0)
 		return (-1);
-	else
-		return (pow (x, y));
-				}
+	else if (y == 0)
+		return (1);
+	else if (y == 1)
+		return (x);
+	return (x * pow_recursion (x, y - 1));
+}
