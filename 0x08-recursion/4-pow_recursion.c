defd1d6763110512a998303be90790ef05ae7f31
ZoLa <zolaabe506@gmail.com>
2022-11-03 07:50:06 -0700

    0x08-recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _pow_recursion(int x, int y) - returns the value of
+ * x raised to the power of y
+ * @x: first number
+ * @y: second number
+ * Return: 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else
+		return (pow (x, y);
+				}
