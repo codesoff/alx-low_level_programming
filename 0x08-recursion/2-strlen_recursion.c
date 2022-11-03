767505a6bd2da9591ede1f4f12bd597bdfa2eaa6
ZoLa <zolaabe506@gmail.com>
2022-11-03 06:58:39 -0700

    0x08-recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * int _strlen_recursion - that returns length of the string
+ * @s: string pointer
+ * Return: 0
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
