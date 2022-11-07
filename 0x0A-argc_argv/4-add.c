42f4b1a9809c42c73400040e1df380f1eba01419
ZoLa <zolaabe506@gmail.com>
2022-11-07 05:57:26 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 1 if error else 0
+ */
+int main(int argc, char *argv[])
+{
+	int total, i;
+	char *p;
+	int num;
+
+	total = 0;
+	if (argc > 1)
+	{
+		for (i = 1; argv[i]; i++)
+		{
+			num = strtol(argv[i], &p, 10);
+			if (!*p)
+				total += num;
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	}
+	printf("%d\n", total);
+	return (0);
+}
