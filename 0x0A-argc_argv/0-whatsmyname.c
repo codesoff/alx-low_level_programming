0086d77840b9415754348bd1295cd99649903549
ZoLa <zolaabe506@gmail.com>
2022-11-07 03:32:52 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,9 +5,8 @@
  * @argv: passed to main as an array of strings
  * Return: 0
  */
-int main(int argc, char **argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
-	void(argc);
 	printf("%s\n", argv[0]);
 	return (0);
 }
