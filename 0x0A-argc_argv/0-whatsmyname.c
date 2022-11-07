3ce5e1826ec0f5ba494014ca686e573330fed6ed
ZoLa <zolaabe506@gmail.com>
2022-11-06 22:12:25 -0800

    0x0A-argc_argv

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - program that prints its name
+ * @argc: argument count
+ * @argv: passed to main as an array of strings
+ * Return: 0
+ */
 int main(int argc, char ** argv)
 {
 	printf("%s", argv[0]);
