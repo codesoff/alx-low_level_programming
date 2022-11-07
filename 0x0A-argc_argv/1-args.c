751389a1977bce168afa69478bc6dc7fde60b820
ZoLa <zolaabe506@gmail.com>
2022-11-06 23:47:36 -0800

    0x0A-argc_argv

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+/**
+ * main - that prints the number of arguments passed into it
+ * @argc: argument count
+ * @srgv: arguments passed to main as an array of strings
+ * Return: 0
+ */
+int main(
