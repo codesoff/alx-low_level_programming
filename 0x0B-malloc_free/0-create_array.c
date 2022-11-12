46a083d255f8e05d66116523f7639cd9cea0133b
ZoLa <zolaabe506@gmail.com>
2022-11-12 06:47:15 -0800

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -14,10 +14,10 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	a = malloc(size * (sizeof(*a));
-			if (a == NULL)
-			return (NULL);
-			for (i = 0; i < size; i++)
-			a[i] = c;
-			return (a);
-			}
+	a = malloc(size * (sizeof(*a)));
+	if (a == NULL)
+		return (NULL);
+	for (i = 0; i < size; i++)
+		a[i] = c;
+	return (a);
+}
